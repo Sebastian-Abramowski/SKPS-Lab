c2ab5495c8d99fb095e280f5b87015f20a8975ab
bstoma <01178601@pw.edu.pl>
2024-03-19 16:59:14 +0100

    Replace zad_domowe_2.py

--- cw2/zad_domowe_2.py
+++ cw2/zad_domowe_2.py
@@ -2,27 +2,36 @@ import math
 import time
 from matplotlib import pyplot as plt
 
-MIN_HZ = 30
-MAX_HZ = 1000
 ITERATIONS = 41
 MULTIPLIER = math.pi / 40
+
+
+def pwm_signal(min_hz, max_hz, step):
+    start = time.time()
+    signal = 1
+    x_axis = []
+    y_axis = []
+    freq = min_hz
+    while freq >= min_hz - step:
+        if freq > max_hz:
+            step = -step
+
+        time.sleep(1 / freq)
+        x_axis.append(time.time() - start)
+        signal = signal ^ 1
+        y_axis.append(signal)
+        freq += step
+
+    return x_axis, y_axis
+
+
+signal = pwm_signal(30, 1000, 90)
+plt.step(signal[0], signal[1])
+plt.title("Sygnał liniowy 30Hz-1000Hz-30Hz")
+plt.savefig("zad2_a.png")
+
 x_axis = []
 y_axis = []
-freqs = []
-
-for freq in range(MIN_HZ, MAX_HZ):
-    freqs.append(freq)
-    # pwm(freq)
-for freq in range(MAX_HZ, MIN_HZ, -1):
-    freqs.append(freq)
-    # pwm(freq)
-
-plt.plot(freqs)
-plt.title("Sygnał liniowy 30Hz-1000Hz-30Hz")
-plt.savefig("zad1.png")
-plt.show()
-
-
 for i in range(ITERATIONS):
     x = i
     y = abs(math.sin(MULTIPLIER * i)) * 0.8 + 0.1
@@ -32,5 +41,4 @@ for i in range(ITERATIONS):
 
 plt.scatter(x_axis, y_axis)
 plt.title(f"Sygnał sinusowy 10%-90%-10%")
-plt.savefig("zad2.png")
-plt.show()
+plt.savefig("zad2_b.png")
