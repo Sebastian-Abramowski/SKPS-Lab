e53e3af53610b08d81d5cf0ca67034d0d00b0fa8
bstoma <01178601@pw.edu.pl>
2024-03-20 13:48:27 +0100

    Added zad3

---
+++ cw2/zad3.py
@@ -0,0 +1,20 @@
+from gpio4 import SysfsGPIO
+from time import sleep
+import time
+
+gpio_led = SysfsGPIO(27)
+gpio_led.export = True
+gpio_led.direction = 'out'
+
+gpio_button = SysfsGPIO(10)
+gpio_button.export = True
+gpio_button.direction = 'in'
+
+gpio_led.value = 0
+
+while True:
+        if gpio_button.value == 0:
+                gpio_led.value = 1 - gpio_led.value
+                time.sleep(0.5)
+gpio_led.export = False
+gpio_button.export = False
