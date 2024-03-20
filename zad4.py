0d9ca9c4a6a731e3ad382932c6896c5d9e6a0836
bstoma <01178601@pw.edu.pl>
2024-03-20 14:35:28 +0100

    Update zad4.py

--- zad4.py
+++ zad4.py
@@ -1,21 +1,22 @@
-from periphery import GPIO, sleep_ms
-from time import time, sleep
+import time
+import gpio4
 
-output = GPIO("/dev/gpiochip0", 18, "out")
-frequencies = [261.63, 293.66, 329.63, 349.23, 392., 440., 493.88, 523.25, 587.33, 659.25, 698.46, 783.99, 880, 987.77, 1046.50]
+buzzer = gpio4.SysfsGPIO(17)
+buzzer.export = True
+buzzer.direction = 'out'
 
-dt = 1e-4
+frequencies = [261.63, 293.66, 329.63, 349.23, 392, 440, 493.88, 523.25, 587.33, 659.25, 698.46, 783.99, 880, 987.77]
+
 duty_cycle = 0.5
 
 for freq in frequencies:
-    start = time()
-    print(freq)
+    start = time.time()
     stop = 0
     while (stop - start < 1):
-        output.write(True)
-        sleep(duty_cycle/freq)
-        output.write(False)
-        sleep(duty_cycle/freq)
-        stop = time()
+        buzzer.value = 1
+        time.sleep(duty_cycle/freq)
+        buzzer.value = 0
+        time.sleep(duty_cycle/freq)
+        stop = time.time()
 
-output.close()
+buzzer.export = False
