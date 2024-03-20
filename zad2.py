e1cd25151a180096998791daa62dc3163649fc66
Sebastian <sebastian.abramowski@onet.pl>
2024-03-20 13:15:49 +0100

    Added zad2.py

---
+++ zad2.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+
+duration = 10
+epsilon = 1e-6
+
+
+def calc_periods(frequency, duty_cycle):
+    period = 1 / frequency
+    high_signal_period = period * duty_cycle
+    low_signal_period = period - high_signal_period
+    return high_signal_period, low_signal_period
+
+
+def variable_duty_cycle(time, duration, min_duty=0.1, max_duty=0.9):
+    half_duration = duration / 2
+    if time <= half_duration:
+        return min_duty + (max_duty - min_duty) * (time / half_duration)
+    else:
+        return max_duty - (max_duty - min_duty) * (
+            (time - half_duration) / half_duration)
+
+
+def generate_values_for_fill_plot(frequency=5):
+    values = []
+    times = []
+    current_t = 0
+    while current_t < duration:
+        variable_duty = variable_duty_cycle(current_t, duration)
+        high_period, low_period = calc_periods(frequency, variable_duty)
+        values.append(1)
+        times.append(current_t)
+        values.append(1)
+        times.append(current_t + high_period)
+        values.append(0)
+        times.append(current_t + high_period + epsilon)
+        values.append(0)
+        times.append(current_t + high_period + epsilon + low_period)
+        current_t += high_period + epsilon + low_period
+    return times, values
+
+
+times, values = generate_values_for_fill_plot()
+plt.plot(times, values)
+plt.xlabel("Czas [s]")
+plt.ylabel("Sygnał PWM")
+plt.title("Sygnał wypełnienie 10%-90%-10%")
+plt.style.use("fivethirtyeight")
+plt.grid(True)
+plt.show()
